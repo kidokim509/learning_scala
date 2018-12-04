cdc869c9b25e5fec0860f32c66901e6c5f341313
olaf.kido <olaf.kido@kakaocorp.com>
2018-12-04 19:46:20 +0900

    TIL-2018-12-04

---
+++ src/main/scala/Chap07HighOrderFunctions.scala
@@ -0,0 +1,67 @@
+/**
+  * https://docs.scala-lang.org/tour/higher-order-functions.html
+  */
+object Chap07HighOrderFunctions {
+
+  // Higher order functions take other functions as parameters or return a function as a result.
+  // This is possible because functions are first-class values in Scala.
+  val salaries = Seq(20000, 70000, 40000)
+  val doubleSalary = (x: Int) => x * 2
+  val newSalaries = salaries.map(doubleSalary) // List(40000, 140000, 80000)
+
+  val shortSalaries = salaries.map(x => x * 2) // List(40000, 140000, 80000)
+
+  val shorterSalaries = salaries.map(_ * 2)
+
+
+  // Coercing methods into functions
+  case class WeeklyWeatherForecast(temperatures: Seq[Double]) {
+
+    private def convertCtoF(temp: Double) = temp * 1.8 + 32
+
+    def forecastInFahrenheit: Seq[Double] = temperatures.map(convertCtoF) // <-- passing the method convertCtoF
+  }
+
+  // Functions that accept functions
+  /*
+  object SalaryRaiser {
+
+    def smallPromotion(salaries: List[Double]): List[Double] =
+      salaries.map(salary => salary * 1.1)
+
+    def greatPromotion(salaries: List[Double]): List[Double] =
+      salaries.map(salary => salary * math.log(salary))
+
+    def hugePromotion(salaries: List[Double]): List[Double] =
+      salaries.map(salary => salary * salary)
+  }
+  */
+
+  object SalaryRaiser {
+
+    private def promotion(salaries: List[Double], promotionFunction: Double => Double): List[Double] =
+      salaries.map(promotionFunction)
+
+    def smallPromotion(salaries: List[Double]): List[Double] =
+      promotion(salaries, salary => salary * 1.1)
+
+    def bigPromotion(salaries: List[Double]): List[Double] =
+      promotion(salaries, salary => salary * math.log(salary))
+
+    def hugePromotion(salaries: List[Double]): List[Double] =
+      promotion(salaries, salary => salary * salary)
+  }
+
+  // Functions that return functions
+  def urlBuilder(ssl: Boolean, domainName: String): (String, String) => String = {
+    val schema = if (ssl) "https://" else "http://"
+    (endpoint: String, query: String) => s"$schema$domainName/$endpoint?$query"
+  }
+
+  val domainName = "www.example.com"
+  def getURL = urlBuilder(ssl=true, domainName)
+  val endpoint = "users"
+  val query = "id=1"
+  val url = getURL(endpoint, query) // "https://www.example.com/users?id=1": String
+
+}
